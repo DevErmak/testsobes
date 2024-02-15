012aa18769341ef3eb3b285a18048b708f1f4ada
DevErmak <o.khasanov.dev@gmail.com>
2024-02-15 17:29:29 +0300

    save data

--- sever/server.js
+++ sever/server.js
@@ -7,7 +7,25 @@ const HOST = "localhost";
 
 app.use(express.json());
 
-app.post("/api/getToken", (req, res) => {});
+app.post("/api/getToken", (req, res) => {
+  console.log("---------------->req", req);
+  // const { data } = await axios.post(
+  //     "http://localhost:3000/api/getToken",
+  //     {
+  //         req.body,
+  //     },
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Origin": "*",
+  //         "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+  //         "Access-Control-Allow-Headers":
+  //           "Accept, X-Requested-With, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+  //       },
+  //     }
+  //   );
+  //   console.log("---------------->data", data);
+});
 
 app.listen(PORT, HOST, () => {
   console.log(`Starting Proxy at ${HOST}:${PORT}`);
