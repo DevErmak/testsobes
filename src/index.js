460a72dec8fbceff6477528fae7996f9b2c0e85f
DevErmak <o.khasanov.dev@gmail.com>
2024-02-16 17:58:40 +0300

    save data

--- src/index.js
+++ src/index.js
@@ -1,23 +1,131 @@
-const link = "";
-const authorization_code = "";
-const access_token = "";
-const refresh_token = "";
-console.log("---------------->123");
+let pageNumber = 1;
+let stateDataDeal = [];
 
-let bodyGetTokens = {
-  client_id: "26fe2bf5-6a9f-4807-b226-66b03f5dad97",
+// const renderDataDeal = () => {
+//   const s = stateDataDeal
+//     .map((deal) => {
+//       return `<tr>
+//     <td>${deal.name}</td>
+//     <td>${deal.price}</td>
+//     <td>${deal.created_at}/${deal.updated_at}</td>
+//     <td>${deal.responsible_user_id}</td>
+//     </tr>`;
+//     })
+//     .join("");
+//   console.log("---------------->s", s);
+//   return s;
+// };
+
+const markupPagination = () => {
+  return `<div class="bottom-field">
+        <ul class="pagination">
+          <li class="prev"><a href="#" id="prev">&#139;</a></li>
+            <!-- page number here -->
+          <li class="next"><a href="#" id="next">&#155;</a></li>
+        </ul>
+    </div>`;
+};
+
+let userData = {
+  client_id: "756a610b-eb07-4014-8f1d-634f54d38af7",
   client_secret:
-    "Xt5FETKcgktn4vHgZK8DuSsTefk8Q5l5SnaM3t3AjQ7y2uFSbCZy6Jl254PU8jTz",
-  grant_type: "authorization_code",
-  code: "def5020081163654fd9b94aeea7cb6cda1cadf7577e13765909ce6157a5ee73f8e48a4740b272eeb78fa74644ebd8159b5db80725e4db9c64eb5862a8f0095a4a718e309136321438d59db877c0c865a0dedc45fe5197b79db8164b5f53f8a5aafb30e13cf8725fbfc0dbd4b4c91bf6468e92a2721768b2f46dac20c46632ade7c893df2b79a14e0514cb390ebf05374e84cc6717f9e00e3d2145db43166960a7f068c7f8392f2285abb2017ea21c69a32fd439bfad5adcda39a410db586bdfe4ac90585fbf82afb8fd8ddd37439137cad850ba7ad8729c28ff716f0cc4bc9ccd24115802d9946908844d328c331b8db17d148e82741bb35dc86978f92e0f99d10ab52c03cdc246b6aa571d18890d02dd3b6fb2911ed9c250c1f85fb3dffe8bf9fd3d41cc5a63f86f57eaf63dd22134c37681cc643860b2cc6add25a79e052ca21fb8b8daba922f17e01b78267d509072e134eea9312669c9c51c24a02f3958cb72224a66f73080c1604a841853c468d34c244ef71ec13d5d686399611807c4f940265c3e02d2ed20831c6f6d3de5b6e9fa94f7017783d44e2b3461afeb68923a693670e27c89fde9d03ddd4853d0dff84da2491647de61e0d72576b7a5cf3a5d354ae8207cbc70f0e9a6618422df0c4096fa582a32d2ec710e3569cda1adebe667bb09986aa",
-  redirect_uri: "http://localhost:3000",
+    "iQq3kmWaZWQDSyWvf3ByMiAT2BTXTV8EYgP0Zen7mP7VVWjNvHZWgVLFWI0CrJy6",
+};
+const CODE_AUTH =
+  "def50200134bf5df3b6f14fa9d05a061d58750c3ee1fdac7c38b7380fe8da0dfc3f840fd897936fe531bc0d2809edde492fbe1fe2c14289402a4a4240c93a133716b22b0ad55cdb1f91f314cf04cf9235a4e5c8aebdafb0348fd6bb39d4b2434da08aa7b2d1e947277a018af226d08423febd6bfd8ed40e869ebf108e069cd486662e99ca236723b8ab02da17b96ff0effedb066cbf2e1f0fcee0d1a185f5b106f6b9fd6b86ba01855fcbf0cabfbe7b423393ab8bd54e696e8d5be24fd6aa50152e2cfbcdd26416cd48701f829e8b0af4013b65e7f9cec4f9c3ba2c635dbdac1b368d31c913cf998b35c31b2910427a83d4ee113625d5ce2a006ef58e2a37e356c72a455bf45367c8fa106cf604eeca76c5c1dd7258c20a15a3217800a2a7bbf349f4b5400619ccbe77795c06cea5574bea69c7e930dce85343cae39a84da46b8c048e7bf9d4843c94b0cc3cc4c731ebdc868674dc3f08bb832aaf8fc45111f0fb30d42bf7be6dc7c1fa9d8f0fd38f742841d1fed3e36a8ffe256d04cc419479f44ac6bafb988f9a2e407b11e5d9516e2578c65aa35669da7e23a31f853894e248d3bbba9ec2e0a643d19b16db5a57275eb471601aaecba96ec770f022dd8f8f4ae65a912b7899eef6651ee4a0146c4014fde9c5b26f072c314b7f09b9af7b131af8bab4457204716e2589b38d7b2cc598";
+
+let dataToken = {
+  access_token: "",
+  refresh_token: "",
+  expires_in: null,
+  token_type: "",
 };
 
 const getToken = async () => {
+  let result = {};
+  console.log(
+    "---------------->dataToken.access_token",
+    dataToken.access_token
+  );
+  if (dataToken.access_token !== "") {
+    if (Date.now() >= dataToken.expires_in * 1000) {
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3000/api/refreshToken",
+          {
+            ...userData,
+            refresh_token: dataToken.refresh_token,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+              "Access-Control-Allow-Headers":
+                "Accept, X-Requested-With, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+            },
+          }
+        );
+        result = data;
+
+        const { access_token, refresh_token, expires_in, token_type } = data;
+        dataToken = {
+          ...dataToken,
+          access_token,
+          refresh_token,
+          expires_in,
+          token_type,
+        };
+      } catch (err) {
+        console.log("---------------->not have token", err);
+      }
+    }
+  } else {
+    try {
+      console.log("---------------->getTokem metig");
+      const { data } = await axios.post(
+        "http://localhost:3000/api/getToken",
+        {
+          ...userData,
+          code: CODE_AUTH,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers":
+              "Accept, X-Requested-With, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+          },
+        }
+      );
+      result = data;
+      console.log("---------------->!!!!data", data);
+
+      const { access_token, refresh_token, expires_in, token_type } = data;
+      dataToken = {
+        ...dataToken,
+        access_token,
+        refresh_token,
+        expires_in,
+        token_type,
+      };
+    } catch (err) {
+      console.log("---------------->not have token", err);
+    }
+  }
+  return result;
+};
+const getDataDeal = async (dataParams) => {
   const { data } = await axios.post(
-    "http://localhost:3000/api/getToken",
+    "http://localhost:3000/api/getDeal",
     {
-      bodyGetTokens,
+      params: {
+        limit: dataParams.limit,
+        page: dataParams.page,
+        // `order[${updated_at}]`: dataParams,
+      },
+      token: dataToken.access_token,
     },
     {
       headers: {
@@ -29,13 +137,111 @@ const getToken = async () => {
       },
     }
   );
-  console.log("---------------->data", data);
+  console.log("---------------->data111s", data);
+  if (data !== "") {
+    const deals = data._embedded.leads;
+    const currentArray = deals.map((deal) => ({
+      name: deal.name,
+      price: deal.price,
+      created_at: parseDate(deal.created_at),
+      updated_at: parseDate(deal.updated_at),
+      responsible_user_id: deal.responsible_user_id,
+    }));
+    stateDataDeal = [...currentArray];
+  }
+  return data;
 };
-getToken();
 
-const root = document.getElementById("root");
-
-const renderComponent = () => {
-  root.innerHTML = "afa";
+const parseDate = (data) => {
+  return `${new Date(data * 1000).toLocaleTimeString()} ${new Date(
+    data * 1000
+  ).toLocaleDateString()}`;
 };
-renderComponent();
+
+const getDataUser = async (dataParams) => {
+  const { data } = await axios.post(
+    "http://localhost:3000/api/getDeal",
+    {
+      params: {
+        user_id: dataParams.user_id,
+      },
+      token: dataToken.access_token,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Accept, X-Requested-With, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+      },
+    }
+  );
+  console.log("---------------->dataUser111s", data);
+  return data;
+};
+
+const itemPerPage = document.getElementById("itemperpage");
+
+itemPerPage.addEventListener("change", async () => {
+  stateDataDeal = [];
+  pageNumber = 1;
+  console.log("Vanilla change: ", itemPerPage.value);
+  await getDataDeal({ limit: itemPerPage.value, page: pageNumber });
+  updateTable();
+});
+
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
+
+btnPrev.addEventListener("click", async () => {
+  stateDataDeal = [];
+  console.log("pageNumber", pageNumber);
+  if (pageNumber > 1) pageNumber--;
+  await getDataDeal({ limit: itemPerPage.value, page: pageNumber });
+  updateTable();
+});
+btnNext.addEventListener("click", async () => {
+  pageNumber++;
+  console.log("pageNumbernex", pageNumber);
+  // if (pageNumber < maxPage) pageNumber++;
+  const data = await getDataDeal({
+    limit: itemPerPage.value,
+    page: pageNumber,
+  });
+  if (data === "") {
+    pageNumber--;
+    await getDataDeal({
+      limit: itemPerPage.value,
+      page: pageNumber,
+    });
+  }
+  updateTable();
+});
+
+function updateTable() {
+  const tbody = document.getElementById("tableDataDeal");
+  console.log("---------------->tbody", tbody);
+  tbody.innerHTML = "";
+  console.log("---------------->stateDataDeal", stateDataDeal);
+  stateDataDeal.forEach((deal) => {
+    const row = document.createElement("tr");
+    for (const key in deal) {
+      const cell = document.createElement("td");
+      cell.textContent = deal[key];
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
+  });
+}
+
+const main = async () => {
+  await getToken();
+  console.log("access_token", dataToken.access_token);
+  console.log("refresh_token", dataToken.refresh_token);
+  console.log("---------------->itemPerPage", itemPerPage.value);
+  await getDataDeal({ limit: itemPerPage.value, page: pageNumber });
+  updateTable();
+};
+
+main();
